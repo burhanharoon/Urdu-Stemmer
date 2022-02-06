0eb977fa249a5e19ab874e39aa66f46b6bead3d3
Burhan <2019cs626@student.uet.edu.pk>
2022-02-06 23:24:24 +0500

    Added new suffixes

--- stemmer.py
+++ stemmer.py
@@ -1,12 +1,20 @@
 import re
 
 allUrduAffixes = {}
+totalWords = 0
+totalCorrectGuessed = 0
 
 urduPrefixes = ['بے', 'بد', 'لا', 'ے', 'نا', 'با', 'کم', 'ان', 'اہل', 'کم']
-urduSuffixes = ['دار', 'وں', 'یاں', 'یں', 'ات', 'گوار', 'ور']
+urduSuffixes = ['دار', 'وں', 'یاں', 'یں', 'ات', 'گوار', 'ور', 'پسند']
+
+
+def remove(string):
+    return string.replace(" ", "")
+
 
 urduFile = open("urdu-affixes.txt", "r", encoding="utf-8")
 for urduWord in urduFile:
+    totalWords = totalWords + 1
     x = urduWord.splitlines()
     x = x[0].split('\t\t')
     allUrduAffixes[x[0]] = x[1]
@@ -14,21 +22,34 @@ for urduWord in urduFile:
 for sentence in allUrduAffixes:
     urduWord = sentence
     prefixFound = False
+
     for prefix in urduPrefixes:
         checkPrefix = re.search(rf'\A{prefix}', urduWord)
         if checkPrefix:
-            print(urduWord[checkPrefix.span(0)[1]:])
+            predictedStem = urduWord[checkPrefix.span(0)[1]:]
+            # print(predictedStem)
             prefixFound = True
+            realStem = remove(allUrduAffixes[sentence])
+            predictedStem = remove(predictedStem)
+            if predictedStem == realStem:
+                totalCorrectGuessed = totalCorrectGuessed + 1
+            # else:
+            #     print(realStem, predictedStem)
             break
+
     if not prefixFound:
         for suffix in urduSuffixes:
             checkSuffix = re.search(rf"{suffix}\Z", urduWord)
             if checkSuffix:
-                print(urduWord[:checkSuffix.span(0)[0]])
+                predictedStem = urduWord[:checkSuffix.span(0)[0]]
+                # print(predictedStem)
+                realStem = remove(allUrduAffixes[sentence])
+                predictedStem = remove(predictedStem)
+                if predictedStem == realStem:
+                    totalCorrectGuessed = totalCorrectGuessed + 1
+                # else:
+                #     print(realStem, predictedStem)
                 break
 
-    # # checkPrefix = re.search('\Aلا', urduWord)
-    # checkSuffix = re.search("دار\Z", urduWord)
-    # if checkSuffix:
-    #     print(urduWord[:checkSuffix.span(0)[0]])
-    #     # print(urduWord[checkPrefix.span(0)[1]:])
+print(totalWords)
+print(totalCorrectGuessed)
