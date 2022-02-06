172e02bd28337c8335d8b9a457e23cb4b32bdc98
Burhan <2019cs626@student.uet.edu.pk>
2022-02-06 22:32:06 +0500

    Added new prefixes

--- stemmer.py
+++ stemmer.py
@@ -1,18 +1,9 @@
 import re
 
-# print(checkWords('بے', 'بےایمان', 'prefix'))
-# print(checkWords('بد', 'بدمزاج', 'prefix'))
-# print(checkWords('لا', 'لاجواب', 'prefix'))
-# print(checkWords('ے', 'بلے', 'prefix'))
-# print(checkWords('نا', 'ناخوص', 'prefix'))
-# print(checkWords('یاں', 'بلیاں', 'suffix'))
-# print(checkWords('وں', 'علاقوں', 'suffix'))
-# print(checkWords('دار', 'ایماندار'))
-
 allUrduAffixes = {}
 
 urduPrefixes = ['بے', 'بد', 'لا', 'ے', 'نا', 'با', 'کم', 'ان', 'اہل', 'کم']
-urduSuffixes = ['دار', 'وں', 'یاں', 'یں', 'ات', 'گوار']
+urduSuffixes = ['دار', 'وں', 'یاں', 'یں', 'ات', 'گوار', 'ور']
 
 urduFile = open("urdu-affixes.txt", "r", encoding="utf-8")
 for urduWord in urduFile:
@@ -20,10 +11,24 @@ for urduWord in urduFile:
     x = x[0].split('\t\t')
     allUrduAffixes[x[0]] = x[1]
 
+for sentence in allUrduAffixes:
+    urduWord = sentence
+    prefixFound = False
+    for prefix in urduPrefixes:
+        checkPrefix = re.search(rf'\A{prefix}', urduWord)
+        if checkPrefix:
+            print(urduWord[checkPrefix.span(0)[1]:])
+            prefixFound = True
+            break
+    if not prefixFound:
+        for suffix in urduSuffixes:
+            checkSuffix = re.search(rf"{suffix}\Z", urduWord)
+            if checkSuffix:
+                print(urduWord[:checkSuffix.span(0)[0]])
+                break
 
-urduWord = 'ایماندار'
-# checkPrefix = re.search('\Aلا', urduWord)
-checkSuffix = re.search("دار\Z", urduWord)
-if checkSuffix:
-    print(urduWord[:checkSuffix.span(0)[0]])
-    # print(urduWord[checkPrefix.span(0)[1]:])
+    # # checkPrefix = re.search('\Aلا', urduWord)
+    # checkSuffix = re.search("دار\Z", urduWord)
+    # if checkSuffix:
+    #     print(urduWord[:checkSuffix.span(0)[0]])
+    #     # print(urduWord[checkPrefix.span(0)[1]:])
